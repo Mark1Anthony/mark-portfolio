11bc40a706791a9690d613ec761f793925c45768
Mark <185191204+Mark1Anthony@users.noreply.github.com>
2026-04-11 15:00:45 -0700

    feat: GitHub Pages config + Actions workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,9 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
+  site: 'https://mark1anthony.github.io',
+  base: '/mark-portfolio',
   integrations: [tailwind()],
   output: 'static',
+  trailingSlash: 'always',
 });
